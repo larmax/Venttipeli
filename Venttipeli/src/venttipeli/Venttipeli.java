68b7e9c726b6a8efd716476648b17fb253f31b11
Miller Leon <s1601385@student.businesscollege.fi>
2017-09-05 14:18:44 +0300

    tein korttipakkaa

--- Venttipeli/src/venttipeli/Venttipeli.java
+++ Venttipeli/src/venttipeli/Venttipeli.java
@@ -5,6 +5,8 @@
  */
 package venttipeli;
 
+import java.util.HashMap;
+
 /**
  *
  * @author s1601385
@@ -12,9 +14,53 @@ package venttipeli;
 public class Venttipeli {
 
     public static void main(String[] args) {
-        System.out.println("moi");
-        System.out.println("mita kuuluu onks niin ihquu mita sulle kuuluu"
-                + "joutsii jou");
+     
+        HashMap<String, String> Kortit = new HashMap<>();
+        
+        Kortit.put("1", "hertta");
+        Kortit.put("2", "hertta");
+Kortit.put("3", "hertta");
+Kortit.put("4", "hertta");
+Kortit.put("5", "hertta");
+Kortit.put("6", "hertta");
+Kortit.put("7", "hertta");
+    Kortit.put("8", "hertta");
+    Kortit.put("9", "hertta");
+    Kortit.put("10", "hertta");
+    Kortit.put("J", "hertta");
+    Kortit.put("Q", "hertta");
+    Kortit.put("K", "hertta");
+    Kortit.put("A", "hertta");
+    
+       Kortit.put("1", "pata");
+        Kortit.put("2", "pata");
+Kortit.put("3", "pata");
+Kortit.put("4", "pata");
+Kortit.put("5", "pata");
+Kortit.put("6", "pata");
+Kortit.put("7", "pata");
+    Kortit.put("8", "pata");
+    Kortit.put("9", "pata");
+    Kortit.put("10", "pata");
+    Kortit.put("J", "pata");
+    Kortit.put("Q", "pata");
+    Kortit.put("K", "pata");
+    Kortit.put("A", "pata");
+        Kortit.put("1", "risti");
+        Kortit.put("2", "risti");
+Kortit.put("3", "risti");
+Kortit.put("4", "risti");
+Kortit.put("5", "risti");
+Kortit.put("6", "risti");
+Kortit.put("7", "risti");
+    Kortit.put("8", "risti");
+    Kortit.put("9", "risti");
+    Kortit.put("10", "risti");
+    Kortit.put("J", "risti");
+    Kortit.put("Q", "risti");
+    Kortit.put("K", "risti");
+    Kortit.put("A", "risti");
+    
     }
     
 }
