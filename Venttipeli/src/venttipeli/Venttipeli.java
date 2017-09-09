8ea47726e60a3bb2dd24631c2d1ef240fc01f63a
Miller Leon <s1601385@student.businesscollege.fi>
2017-09-09 14:08:36 +0300

    melkee valmis

--- Venttipeli/src/venttipeli/Venttipeli.java
+++ Venttipeli/src/venttipeli/Venttipeli.java
@@ -5,62 +5,110 @@
  */
 package venttipeli;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
 
-/**
- *
- * @author s1601385
- */
 public class Venttipeli {
 
     public static void main(String[] args) {
-     
-        HashMap<String, String> Kortit = new HashMap<>();
+
+        ArrayList<Integer> Korttienarvo = new ArrayList<>();
+       
+        String pelaajankortinarvo = "";
+        String pelaajankortinmaa = "";
+        String jakajankortinarvo = "";
+        String jakajankortinmaa = "";
         
-        Kortit.put("1", "hertta");
-        Kortit.put("2", "hertta");
-Kortit.put("3", "hertta");
-Kortit.put("4", "hertta");
-Kortit.put("5", "hertta");
-Kortit.put("6", "hertta");
-Kortit.put("7", "hertta");
-    Kortit.put("8", "hertta");
-    Kortit.put("9", "hertta");
-    Kortit.put("10", "hertta");
-    Kortit.put("J", "hertta");
-    Kortit.put("Q", "hertta");
-    Kortit.put("K", "hertta");
-    Kortit.put("A", "hertta");
-    
-       Kortit.put("1", "pata");
-        Kortit.put("2", "pata");
-Kortit.put("3", "pata");
-Kortit.put("4", "pata");
-Kortit.put("5", "pata");
-Kortit.put("6", "pata");
-Kortit.put("7", "pata");
-    Kortit.put("8", "pata");
-    Kortit.put("9", "pata");
-    Kortit.put("10", "pata");
-    Kortit.put("J", "pata");
-    Kortit.put("Q", "pata");
-    Kortit.put("K", "pata");
-    Kortit.put("A", "pata");
-        Kortit.put("1", "risti");
-        Kortit.put("2", "risti");
-Kortit.put("3", "risti");
-Kortit.put("4", "risti");
-Kortit.put("5", "risti");
-Kortit.put("6", "risti");
-Kortit.put("7", "risti");
-    Kortit.put("8", "risti");
-    Kortit.put("9", "risti");
-    Kortit.put("10", "risti");
-    Kortit.put("J", "risti");
-    Kortit.put("Q", "risti");
-    Kortit.put("K", "risti");
-    Kortit.put("A", "risti");
-    
+        
+         Korttienarvo.add(2);
+         Korttienarvo.add(3);
+         Korttienarvo.add(4);
+        Korttienarvo.add(5);
+         Korttienarvo.add(6);
+        Korttienarvo.add(7);
+       Korttienarvo.add(8);
+        Korttienarvo.add(9);
+         Korttienarvo.add(10);
+       Korttienarvo.add(11);
+         Korttienarvo.add(12);
+        Korttienarvo.add(13);
+        Korttienarvo.add(14);
+        
+         ArrayList<String> Korttienmaa = new ArrayList<>();
+         
+         Korttienmaa.add("Pata");
+         Korttienmaa.add("Risti");
+         Korttienmaa.add("Ruutu");
+         Korttienmaa.add("Hertta");
+        
+       
+    Jakaminen(Korttienarvo, Korttienmaa);
     }
     
+public static void Jakaminen (ArrayList<Integer> Korttienarvo, ArrayList<String> Korttienmaa){
+     Scanner lukijantti = new Scanner(System.in);   
+    Random rand = new Random();
+        
+
+
+     HashMap< Integer,String> Pelaajankäsi = new HashMap<>();
+    for(int i = 0; i<2; i++){
+           int arvo = Korttienarvo.get(rand.nextInt(Korttienarvo.size()));
+
+     String maa = Korttienmaa.get(rand.nextInt(Korttienmaa.size()));
+
+        Pelaajankäsi.put(arvo, maa);
+        
+    }
+    System.out.println("Korttisi" + Pelaajankäsi);
+
+    System.out.println("haluatko lisätä tai jäädä");
+    String lisaajaa = lukijantti.nextLine();
+    
+    if(lisaajaa.equals("lisää") ){
+         int arvoo = Korttienarvo.get(rand.nextInt(Korttienarvo.size()));
+
+     String maaa = Korttienmaa.get(rand.nextInt(Korttienmaa.size()));
+     if(Pelaajankäsi.containsKey(maaa) && Pelaajankäsi.containsValue(arvoo)){
+         arvoo = Korttienarvo.get(rand.nextInt(Korttienarvo.size()));
+         maaa = Korttienmaa.get(rand.nextInt(Korttienmaa.size()));
+           Pelaajankäsi.put(arvoo, maaa);
+           System.out.println("Korttisi"+Pelaajankäsi);
+     }else{
+     Pelaajankäsi.put(arvoo, maaa);
+     System.out.println("Korttisi"+Pelaajankäsi);
+    } 
+    }
+    else if(lisaajaa.equals("jää") ){
+          emanta(Korttienarvo, Korttienmaa);
+            }
+                
 }
+     
+   
+public static void pelaajanKortit(String arvo){
+    
+}
+     public static void emanta(ArrayList<Integer> Korttienarvo, ArrayList<String> Korttienmaa){
+     
+         Random rand = new Random();
+            HashMap< Integer,String> Emännänkäsi = new HashMap<>();
+         int korttienmaara = rand.nextInt((4 - 1));
+            for(int i = 0; i<korttienmaara; i++){   
+         int arvoo = Korttienarvo.get(rand.nextInt(Korttienarvo.size()));
+
+     String maaa = Korttienmaa.get(rand.nextInt(Korttienmaa.size()));
+         
+        Emännänkäsi.put(arvoo, maaa);
+                System.out.println(Emännänkäsi);
+     }
+       
+    }
+    
+
+}
+    
+
